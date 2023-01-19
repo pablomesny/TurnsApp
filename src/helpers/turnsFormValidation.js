6292ce657630c48a5f5afc344967f649ca2752e5
pablomesny <pablomesny@gmail.com>
2023-01-19 18:12:52 -0300

    turns validation small fix

--- src/helpers/turnsFormValidation.js
+++ src/helpers/turnsFormValidation.js
@@ -2,16 +2,16 @@ import Swal from "sweetalert2"
 
 export const turnsFormValidation = ( formValues ) => {
 
-    const getMinutes = Number(new Date(date).toLocaleTimeString().split(':')[1]);
+    const { date, client, description } = formValues;
 
-    console.log(getMinutes);
+    const getMinutes = Number(new Date(date).toLocaleTimeString().split(':')[1]);
 
     if ( client === '' || description === '' ) {
         Swal.fire( 'Error', 'Completar todos los campos obligatorios', 'error');
         return true;
     }
 
-    if ( getMinutes !== 30 && getMinutes !== 0  ) {
+    if ( getMinutes !== 30 && getMinutes !== 0 ) {
         Swal.fire( 'Fecha incorrecta', 'Debe seleccionar una nueva fecha', 'error');
         return true;
     }
