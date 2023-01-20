3bc9cf1a25fa5f1ff694ece5531df41158affa02
pablomesny <pablomesny@gmail.com>
2023-01-20 15:31:58 -0300

    date validations reworked

--- src/helpers/turnsFormValidation.js
+++ src/helpers/turnsFormValidation.js
@@ -4,14 +4,19 @@ export const turnsFormValidation = ( formValues, registeredTurns ) => {
 
     const { date, client, description } = formValues;
 
-    const getMinutes = Number(new Date(date).toLocaleTimeString().split(':')[1]);
+    const dateTime = new Date(date).toLocaleTimeString().split(':');
+    const dateHour = Number(dateTime[0]);
+    const dateMinutes = Number(dateTime[1]);
+    const dateSeconds = Number(dateTime[2]);
+
+    const validDate = dateHour >= 7 && dateHour <= 16 && (dateMinutes === 30 || dateMinutes === 0) && dateSeconds === 0;
 
     if ( client === '' || description === '' ) {
         Swal.fire( 'Error', 'Completar todos los campos obligatorios', 'error');
         return true;
     }
 
-    if ( getMinutes !== 30 && getMinutes !== 0 ) {
+    if ( !validDate ) {
         Swal.fire( 'Fecha incorrecta', 'Debe seleccionar una nueva fecha', 'error');
         return true;
     }
